f7c825a43bf38a2f68a043e7ddee4431e3118b57
Lucas Apablaza <apablaza.lucas2020@gmail.com>
2026-05-15 14:28:58 -0400

    Revert "part5 - exercises"
    This reverts commit 7a52d82de4725541fae32d304406d1ad4cf189f4.

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = (props) => {
+  if (props.all <= 0) return <p>No feedback given</p>;
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive + " %"} />
+      </tbody>
+    </table>
+  );
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const all = good + neutral + bad;
+  const points = good * 1 + neutral * 0 + bad * -1;
+  const average = points / all;
+  const positive = (good * 100) / all;
+
+  return (
+    <div>
+      <div>
+        <h1>Give Feedback</h1>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      </div>
+      <h1>Statistics</h1>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      />
+    </div>
+  );
+}
+
+export default App;
